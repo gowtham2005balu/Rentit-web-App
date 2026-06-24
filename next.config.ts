4b7c6c9d9c5d73e6ea3f4235ab9e75d7aa15c36a
gowtham2005balu <gowthambalu2005@gmail.com>
2026-06-24 12:14:36 +0530

    Fix eslint config warning

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // Removed proxy to Render so that the local Neon database is used!
 };
 
